3b8fc9494bece07a305827b2b4eae2428d7acac2
ManhDevFull <thanhmanhdangfa@gmail.com>
2025-10-18 09:02:36 +0700

    intent

--- app/src/main/java/com/example/giuaki/MainActivity.java
+++ app/src/main/java/com/example/giuaki/MainActivity.java
@@ -1,11 +1,30 @@
 package com.example.giuaki;
 
+import android.content.Intent;
+import com.google.android.material.textfield.TextInputEditText;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+  private TextInputEditText ipPass, ipName;
+  private Button btnLogin;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ipName = findViewById(R.id.ipName);
+    ipPass = findViewById(R.id.ipPass);
+    btnLogin = findViewById(R.id.btnLogin);
+    btnLogin.setOnClickListener(v->{
+      String name = ipName.getText() == null ? "" : ipName.getText().toString().trim();
+      String pass = ipPass.getText() == null ? "" : ipPass.getText().toString().trim();
+      if(name.isEmpty() && pass.isEmpty()){
+        ipName.setError("Login faild !!!");
+        return;
+      }
+      startActivity(new Intent(this, HomeActivity.class));
+    });
   }
 }
